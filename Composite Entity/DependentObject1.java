8d3f618f5d76c869e886be9aab3a6693e69ba2ec
Islam Mesha <eng.islammesha@gmail.com>
2018-08-21 22:18:16 +0200

    Composite Entity Pattern

---
+++ Composite Entity/DependentObject1.java
@@ -0,0 +1,15 @@
+/**
+ * DependentObject1
+ */
+public class DependentObject1 {
+
+    private String data;
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
