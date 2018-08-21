a52762c841b7bdb9f23f44ee29c42b2d2d4452b9
Islam Mesha <eng.islammesha@gmail.com>
2018-08-21 21:24:17 +0200

    Template Pattern

---
+++ Template/Football.java
@@ -0,0 +1,20 @@
+/**
+ * Football
+ */
+public class Football extends Game {
+
+    @Override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    };
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    };
+
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    };
+}
